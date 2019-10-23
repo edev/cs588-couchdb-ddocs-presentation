6f00dadfd491dca2064f6c684efd0b5f0ca9c1a4
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-10-22 17:20:36 -0700

    Added ERB and Markdown rendering for slides
    Also, if a document is not found, returns 404.

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -9,6 +9,8 @@ DDOCS_DIR = './ddocs'
 require 'sinatra'
 require 'net/http'
 require 'json'
+require 'erb'
+require 'redcarpet'
 
 configure do
     set :bind, '0.0.0.0'
@@ -118,19 +120,6 @@ rescue Exception => e
     return e.message
 end
 
-$first_run = true
-get '/' do
-    if $first_run == true
-
-        # Send a temporary redirect to run first-run code, e.g. to create databsaes.
-        redirect "/first_run", 307
-    end
-
-    # Net::HTTP.get(DB_HOST, '/', DB_PORT)
-    navbar = navigation()
-    navbar
-end
-
 get '/first_run' do
     headers "Content-Type" => "text/plain"
     response = ""
@@ -152,4 +141,48 @@ get '/first_run' do
     else
         response
     end
+end
+
+get '/' do
+    show_slide('intro') # TODO Change to redirect to last-viewed page
+end
+
+get '/:docid' do
+    show_slide params[:docid]
+end
+
+$first_run = true
+def show_slide(docid)
+    if $first_run == true
+
+        # Send a temporary redirect to run first-run code, e.g. to create databsaes.
+        redirect "/first_run", 307
+    end
+
+    doc = get_doc(docid)
+
+    navbar = navigation()
+
+
+    markdown_renderer = Redcarpet::Render::HTML.new(render_options = {})
+    markdown = Redcarpet::Markdown.new(markdown_renderer, extensions = {})
+
+    template = ERB.new IO.read('slide.erb')
+    template.result(binding)
+end
+
+def get_doc(docid)
+    uri = URI::HTTP.build(
+        host: DB_HOST,
+        port: DB_PORT,
+        path: '/' + DB_DBNAME + '/' + docid
+    )
+    response = Net::HTTP.get_response(uri)
+    if response.code == "200"
+        return JSON.parse(response.body)
+    else
+        halt response.code
+    end
+rescue Exception => e
+    return e.message
 end
